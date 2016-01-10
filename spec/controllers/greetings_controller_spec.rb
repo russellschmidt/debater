355f18ed25b76660d0ca073d797ae6af2c67c6d2
Russell Schmidt <russell.schmidt@me.com>
2016-01-09 16:52:19 -0800

    make postgres work with rspec

---
+++ spec/controllers/greetings_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe GreetingsController, type: :controller do
+  describe "get HELLO" do
+    it "returns the hello view" do
+      get :hello
+      expect(response).to render_template("hello")
+    end
+  end
+end
