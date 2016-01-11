dc1e6545d348a35b5d40ff0e1110d56cd193f4ab
Russell Schmidt <russell.schmidt@me.com>
2016-01-10 19:59:35 -0800

    change mailer settings to smtp not mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'debater.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.mailgun_settings = {
+  config.action_mailer.smtp_settings = {
     domain:         'debater.herokuapp.com',
     port:           ENV['MAILGUN_SMTP_PORT'],
     address:        ENV['MAILGUN_SERVER'],
