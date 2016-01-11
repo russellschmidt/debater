eb12fa197c60c0f2559bcf3c7f9dbbd622b97cb6
Russell Schmidt <russell.schmidt@me.com>
2016-01-10 19:30:45 -0800

    add config line to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'postmaster@sandboxff2a9f6fa2a5497dbe0b0217da944f7e.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
