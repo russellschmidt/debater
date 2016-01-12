4cde8f297bb1b97f7013ad8b2c900cd73356d3c9
Russell Schmidt <russell.schmidt@me.com>
2016-01-11 21:24:25 -0800

    another futzing with mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    domain:         'debater.herokuapp.com',
+    domain:         ENV['MAILGUN_DOMAIN'],
     port:           ENV['MAILGUN_SMTP_PORT'],
     address:        ENV['MAILGUN_SERVER'],
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-
+    authentication: :plain
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
