449203df3ea33c6f371c294f80d40b4a9fce4cc7
Russell Schmidt <russell.schmidt@me.com>
2016-01-14 08:20:11 -0800

    specs passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111015307) do
+ActiveRecord::Schema.define(version: 20160114155136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "debates", force: :cascade do |t|
+    t.string   "title"
+    t.text     "topic"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "debates", ["user_id"], name: "index_debates_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,4 +49,5 @@ ActiveRecord::Schema.define(version: 20160111015307) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "debates", "users"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:my_user) {create(:user)}
+
+  describe "attributes" do
+    it "should respond to email" do
+      expect(my_user).to respond_to(:email)
+    end
+
+    it "should respond to password" do
+      expect(my_user).to respond_to(:password)
+    end
+
+    it "should respond to confirmed at" do
+      expect(my_user).to respond_to(:confirmed_at)
+    end
+  end
 end
