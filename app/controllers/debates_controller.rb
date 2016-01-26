51d1ecf132fb5c128ae8ad58f4f7a0bee9d611a3
Russell Schmidt <russell.schmidt@me.com>
2016-01-25 21:32:00 -0800

    add all 4 debate views and controller

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -1,25 +1,54 @@
 class DebatesController < ApplicationController
 
   def index
+    @debates = Debate.all
   end
 
   def show
+    @debate = Debate.find(params[:id])
   end
 
   def new
+    @debate = Debate.new
   end
 
   def create
+    @debate = Debate.new(debate_param)
+
+    if @debate.save
+      flash[:notice] = "New debate created"
+      redirect_to :index
+    else
+      flash[:alert] = "Problem with your new debate. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @debate = Debate.find(params[:id])
   end
 
   def update
+    @debate = Debate.find(params[:id])
+
+    if @debate.update_attributes(debate_param)
+      flash[:notice] = "Debate updated"
+      redirect_to debate_path(@debate)
+    else
+      flash[:alert] = "Update didn't work. Please try again."
+      render :edit
+    end
   end
 
   def destroy
+    Debate.find(params[:id]).destroy
+    flash[:notice] = "Debate deleted"
+    redirect_to :index
   end
 
+  private
+  def debate_param
+    params.require(:debates).permit(:resolution, :creator_id)
+  end
 
 end
