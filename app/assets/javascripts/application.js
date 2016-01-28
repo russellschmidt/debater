a1e505b94d36142210085735470470aada74a9ed
Russell Schmidt <russell.schmidt@me.com>
2016-01-28 10:02:00 -0800

    attempt to fix jquery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require materialize-sprockets
 //= require jquery_ujs
+//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
 
