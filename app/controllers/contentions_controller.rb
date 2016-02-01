32140221cb170ecacea30e9698bf45a26b46efc2
Russell Schmidt <russell.schmidt@me.com>
2016-01-31 17:06:01 -0800

    contention model passing tests

---
+++ app/controllers/contentions_controller.rb
@@ -0,0 +1,18 @@
+class ContentionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- spec/models/contention_spec.rb
+++ spec/models/contention_spec.rb
@@ -9,7 +9,23 @@ RSpec.describe Contention, type: :model do
     end
 
     it "responds to summary" do
-      expect(my_contention).to respond_to(:summary)    
+      expect(my_contention).to respond_to(:summary)
+    end
+  end
+
+  context "valid input" do
+    it "is valid with a topic, summary and speech" do
+      expect(my_contention).to be_valid
+    end
+  end
+
+  context "invalid input" do
+    it "is invalid with a nil topic" do
+      bad_contention = Contention.new(
+        topic: nil,
+        summary: "This is a summary of a contention",
+        speech_id: Speech.first)
+      expect(bad_contention).to be_invalid
     end
   end
 end
