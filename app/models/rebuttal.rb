13d6ad321ed5715259494205cb288cf7ecc6b043
Russell Schmidt <russell.schmidt@me.com>
2016-02-04 22:05:57 -0800

    add rebuttal model, delete one-to-many relationship and model attribute

---
+++ app/models/rebuttal.rb
@@ -0,0 +1,2 @@
+class Rebuttal < ActiveRecord::Base
+end
---
+++ db/migrate/20160205054948_remove_column_from_contention.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromContention < ActiveRecord::Migration
+  def change
+    remove_column :contentions, :assertion_id
+  end
+end
