40a838628117bd275598770eaeea7ef3f24ff16b
Russell Schmidt <russell.schmidt@me.com>
2016-02-05 08:59:02 -0800

    add refute button and show opponent contentions in view

--- app/controllers/rebuttals_controller.rb
+++ app/controllers/rebuttals_controller.rb
@@ -1,14 +1,17 @@
 class RebuttalsController < ApplicationController
   before_action :find_debate, only: [:create, :destroy]
+  before_action :find_speech, only: [:create, :destroy]
+  before_action :find_speech_name, only: [:create, :destroy]
 
   def create
     @rebuttal = Rebuttal.new(contention_id: params[:contention_id])
     if @rebuttal.save
       flash[:notice] = "Rebuttal saved successfully"
-      if is_affirmative?
-        redirect_to new_speech_contention(@debate.positions.neg.first.speeches.first)
+      case @speech_name
+      when 'OneNC'
+        redirect_to new_speech_contention_path(@speech)
       else
-        redirect_to new_speech_contention(@debate.positions.aff.first.speeches.first)
+        redirect_to debate_path(@debate)
       end
     else
       flash[:alert] = "Error saving rebuttal"
@@ -32,7 +35,12 @@ class RebuttalsController < ApplicationController
     @debate = params[:debate_id]
   end
 
-  def is_affirmative?
-    @affirmative = params[:affirmative]
+  def find_speech
+    @speech = params[:speech]
   end
+
+  def find_speech_name
+    @speech_name = @speech.speech_name
+  end
+
 end
