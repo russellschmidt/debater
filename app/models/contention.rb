e693fdbb20fdedb0b4df002575c6487091412865
Russell Schmidt <russell.schmidt@me.com>
2016-02-04 19:25:58 -0800

    refactored debate#show with partials

--- app/models/contention.rb
+++ app/models/contention.rb
@@ -1,6 +1,9 @@
 class Contention < ActiveRecord::Base
-  belongs_to :speech
   has_many :arguments, dependent: :destroy
+  belongs_to :speech
+
+  has_many :refutations, class_name: "Contention", foreign_key: "assertion_id"
+  belongs_to :assertion, class_name: "Contention"
 
   validates :topic, presence: true
   validates :speech_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202222221) do
+ActiveRecord::Schema.define(version: 20160204231008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,10 +32,12 @@ ActiveRecord::Schema.define(version: 20160202222221) do
     t.string   "topic"
     t.text     "summary"
     t.integer  "speech_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "assertion_id"
   end
 
+  add_index "contentions", ["assertion_id"], name: "index_contentions_on_assertion_id", using: :btree
   add_index "contentions", ["speech_id"], name: "index_contentions_on_speech_id", using: :btree
 
   create_table "debates", force: :cascade do |t|
