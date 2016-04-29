21c515e478d8085347e29f66b211adc766cb7167
Russell Schmidt <russell.schmidt@me.com>
2016-04-29 10:33:23 -0700

    add sendgrid data replace mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,17 +74,18 @@ Rails.application.configure do
     authentication:   :plain
   }
 =end
-  config.action_mailer.default_url_options = { host: 'debater.herokuapp.com'}
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.smtp_settings = {
-    port:           ENV['MAILGUN_SMTP_PORT'],
-    address:        ENV['MAILGUN_SERVER'],
-    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
-    password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'debater.herokuapp.com',
-    authentication: :plain
-  }
+config.action_mailer.default_url_options = { host: 'debater.herokuapp.com'}
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :sendgrid
+config.action_mailer.smtp_settings = {
+  port:                   '587',
+  address:                'smtp.sendgrid.net',
+  enable_starttls_auto:   true,
+  user_name:              ENV['SENDGRID_USERNAME'],
+  password:               ENV['SENDGRID_PASSWORD'],
+  domain:                 'debater.herokuapp.com',
+  authentication:         :plain
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
